c7f570f40a48456bef547e865a754b70298ceac9
adabalabharath <saibharath.adabala@gmail.com>
2025-09-05 21:57:31 +0530

    email

--- src/components/Bag.jsx
+++ src/components/Bag.jsx
@@ -105,20 +105,20 @@ const Bag = () => {
     };
     console.log(templateParams);
     setDialog(true);
-    // emailjs
-    //   .send(
-    //     "service_z8t1myy", // from EmailJS dashboard
-    //     "template_54rmibg", // from EmailJS dashboard
-    //     templateParams,
-    //     "RlzD4i2llX_Q8d6TV" // from EmailJS dashboard
-    //   )
-    //   .then(() => {
-    //     alert("order placed successfully");
-    //   })
-    //   .catch((error) => {
-    //     console.error(error);
-    //     alert("Failed to send email.");
-    //   });
+    emailjs
+      .send(
+        "service_z8t1myy", // from EmailJS dashboard
+        "template_54rmibg", // from EmailJS dashboard
+        templateParams,
+        "RlzD4i2llX_Q8d6TV" // from EmailJS dashboard
+      )
+      .then(() => {
+        alert("order placed successfully");
+      })
+      .catch((error) => {
+        console.error(error);
+        alert("Failed to send email.");
+      });
   };
 
   const handleChange = (event, product) => {
