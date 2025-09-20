97a5f490c5801f0e3f35fbcddd79180b8d3daf93
adabalabharath <saibharath.adabala@gmail.com>
2025-09-20 23:05:54 +0530

    home swiper

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,20 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import home from "../../public/images/e-commerce-online-shopping-doodle-hand-drawn-icon-set-outline-drawing-e-commerce-online-shopping-line-clipart-symbol-collection-395304571.webp";
 import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
 import { Link } from "react-router-dom";
 import mens from "../../public/images/mens.jpg";
+import mens2 from "../../public/images/mens2.jpg";
+import mens3 from "../../public/images/mens3.jpg";
+import mens4 from "../../public/images/mens4.jpg";
 import women from "../../public/images/women.jpg";
+import women2 from "../../public/images/women2.jpg";
+import women3 from "../../public/images/women3.jpg";
+import women4 from "../../public/images/women4.jpg";
 import kidsSection from "../../public/images/kidsSection.webp";
+import kidsSection2 from "../../public/images/kidSection2.jpg";
+import kidsSection3 from "../../public/images/kidSection3.jpg";
 import homeSection from "../../public/images/homeSection.png";
 import beautySection from "../../public/images/beautySection.jpg";
 import forHim from "../../public/images/forHim.png";
@@ -16,9 +24,17 @@ import forHome from "../../public/images/home.png";
 import forBeauty from "../../public/images/beauty.png";
 import { clearFilters } from "../redux/action";
 import { useDispatch } from "react-redux";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay, Pagination } from "swiper/modules";
 
+const menImages = [mens, mens2, mens3, mens4];
+const womenImages = [women, women2, women3, women4];
+const kidImages = [kidsSection, kidsSection2, kidsSection3];
 const Home = () => {
   const [Hover, setHover] = useState("");
+  const menSwipeRef = useRef(null);
+  const womenSwipeRef = useRef(null);
+  const kidSwipeRef = useRef(null);
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(clearFilters);
@@ -53,7 +69,6 @@ const Home = () => {
           fontWeight: "bold",
           alignItems: "center",
           textAlign: "center",
-          textDecoration: "underline",
         }}
       >
         Men
@@ -89,21 +104,36 @@ const Home = () => {
         m={2}
         sx={{ display: { xs: "none", sm: "none", md: "flex" } }}
       >
-        <Link to={"/shop/men"}>
-          <img
-            src={mens}
-            alt="clothing"
-            style={{
-              width: "1200px",
-              maxHeight: "400px",
-              borderRadius: 10,
-              boxShadow: Hover === "men" ? "50px" : 0,
-              border: Hover === "men" ? "5px solid black" : 0,
-            }}
-            onMouseEnter={() => setHover("men")}
-            onMouseLeave={() => setHover("menLeave")}
-          />
-        </Link>
+        <Swiper
+          spaceBetween={10}
+          slidesPerView={1}
+          pagination={{ clickable: true }}
+          modules={[Pagination, Autoplay]}
+          onSwiper={(swiper) => (menSwipeRef.current = swiper)}
+          autoplay={false}
+          style={{ width: "100%", maxWidth: "1200px", borderRadius: 10 }}
+          loop={true}
+        >
+          {menImages.map((x, i) => (
+            <SwiperSlide key={i}>
+              <Link to={"/shop/men"}>
+                <img
+                  src={x}
+                  alt="clothing"
+                  style={{
+                    width: "100%",
+                    maxHeight: "400px",
+                    borderRadius: 10,
+                    // boxShadow: Hover === "men" ? "50px" : 0,
+                    // border: Hover === "men" ? "5px solid black" : 0,
+                  }}
+                  onMouseEnter={() => menSwipeRef.current?.autoplay.start()}
+                  onMouseLeave={() => menSwipeRef.current?.autoplay.stop()}
+                />
+              </Link>
+            </SwiperSlide>
+          ))}
+        </Swiper>
       </Grid>
 
       <Typography
@@ -113,7 +143,6 @@ const Home = () => {
           fontWeight: "bold",
           alignItems: "center",
           textAlign: "center",
-          textDecoration: "underline",
         }}
       >
         Women
@@ -148,22 +177,41 @@ const Home = () => {
         m={2}
         sx={{ display: { xs: "none", sm: "none", md: "flex" } }}
       >
-        <Link to={"/shop/women"}>
-          <img
-            src={women}
-            alt="clothing"
-            style={{
-              width: "1200px",
-              maxHeight: "400px",
-              borderRadius: 10,
-              transition: "all 0.1s ease",
-              boxShadow: Hover === "women" ? "50px" : 0,
-              border: Hover === "women" ? "5px solid black" : 0,
-            }}
-            onMouseEnter={() => setHover("women")}
-            onMouseLeave={() => setHover("womenLeave")}
-          />
-        </Link>
+        <Swiper
+          spaceBetween={10}
+          slidesPerView={1}
+          pagination={{ clickable: true }}
+          // autoplay={{
+          //   delay: 3000,
+          //   disableOnInteraction: false,
+          //   reverseDirection:true
+          // }}
+          modules={[Pagination, Autoplay]}
+          onSwiper={(swiper) => (womenSwipeRef.current = swiper)}
+          autoplay={false}
+          style={{ width: "100%", maxWidth: "1200px", borderRadius: 10 }}
+          loop={true}
+        >
+          {womenImages.map((x, i) => (
+            <SwiperSlide key={i}>
+              <Link to={"/shop/women"}>
+                <img
+                  src={x}
+                  alt="clothing"
+                  style={{
+                    width: "100%",
+                    maxHeight: "400px",
+                    borderRadius: 10,
+                    // boxShadow: Hover === "women" ? "50px" : 0,
+                    // border: Hover === "women" ? "5px solid black" : 0,
+                  }}
+                  onMouseEnter={() => womenSwipeRef.current?.autoplay.start()}
+                  onMouseLeave={() => womenSwipeRef.current?.autoplay.stop()}
+                />
+              </Link>
+            </SwiperSlide>
+          ))}
+        </Swiper>
       </Grid>
 
       <Typography
@@ -173,7 +221,6 @@ const Home = () => {
           fontWeight: "bold",
           alignItems: "center",
           textAlign: "center",
-          textDecoration: "underline",
         }}
       >
         Kids
@@ -207,22 +254,41 @@ const Home = () => {
         m={2}
         sx={{ display: { xs: "none", sm: "none", md: "flex" } }}
       >
-        <Link to={"/shop/kids"}>
-          <img
-            src={kidsSection}
-            alt="clothing"
-            style={{
-              width: "1200px",
-              maxHeight: "400px",
-              borderRadius: 10,
-              transition: "all 0.1s ease",
-              boxShadow: Hover === "kids" ? "50px" : 0,
-              border: Hover === "kids" ? "5px solid black" : 0,
-            }}
-            onMouseEnter={() => setHover("kids")}
-            onMouseLeave={() => setHover("kidsLeave")}
-          />
-        </Link>
+        <Swiper
+          spaceBetween={10}
+          slidesPerView={1}
+          pagination={{ clickable: true }}
+          // autoplay={{
+          //   delay: 3000,
+          //   disableOnInteraction: false,
+          //   reverseDirection:true
+          // }}
+          modules={[Pagination, Autoplay]}
+          onSwiper={(swiper) => (kidSwipeRef.current = swiper)}
+          autoplay={false}
+          style={{ width: "100%", maxWidth: "1200px", borderRadius: 10 }}
+          loop={true}
+        >
+          {kidImages.map((x, i) => (
+            <SwiperSlide key={i}>
+              <Link to={"/shop/kids"}>
+                <img
+                  src={x}
+                  alt="clothing"
+                  style={{
+                    width: "100%",
+                    maxHeight: "400px",
+                    borderRadius: 10,
+                    // boxShadow: Hover === "women" ? "50px" : 0,
+                    // border: Hover === "women" ? "5px solid black" : 0,
+                  }}
+                  onMouseEnter={() => kidSwipeRef.current?.autoplay.start()}
+                  onMouseLeave={() => kidSwipeRef.current?.autoplay.stop()}
+                />
+              </Link>
+            </SwiperSlide>
+          ))}
+        </Swiper>
       </Grid>
 
       <Typography
@@ -232,7 +298,6 @@ const Home = () => {
           fontWeight: "bold",
           alignItems: "center",
           textAlign: "center",
-          textDecoration: "underline",
         }}
       >
         Home
@@ -292,7 +357,6 @@ const Home = () => {
           fontWeight: "bold",
           alignItems: "center",
           textAlign: "center",
-          textDecoration: "underline",
         }}
       >
         Beauty
